79b41f35039f3824b734909664cc567d26e4d837
tunogya <20251942+tunogya@users.noreply.github.com>
2023-12-26 18:08:59 +0800

    update

--- configs/env.go
+++ configs/env.go
@@ -20,12 +20,6 @@ func EnvMongoURI() string {
 	return url
 }
 
-func EnvRedisURI() string {
-	// get url from .env
-	url := os.Getenv("REDIS_URI")
-	return url
-}
-
 func EnvMysqlURI() string {
 	url := os.Getenv("DSN")
 	return url
