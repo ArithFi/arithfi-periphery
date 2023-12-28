ecfd1add9f21a920027f81307cbfa2c83e5f7d70
tunogya <20251942+tunogya@users.noreply.github.com>
2023-12-28 18:06:00 +0800

    update

--- scripts/jobs.go
+++ scripts/jobs.go
@@ -1,41 +1,49 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
-	"strings"
+	"time"
 )
 
 func main() {
+	// Start and end dates for the range
+	startDate := time.Date(2023, 9, 30, 0, 0, 0, 0, time.UTC)
+	endDate := time.Date(2023, 12, 28, 0, 0, 0, 0, time.UTC)
 
-	url := "http://localhost:8080/fapi/update_daily_deposit_withdraw_metrics"
-	method := "POST"
+	// Iterate over each day in the range
+	for date := startDate; date.Before(endDate); date = date.AddDate(0, 0, 1) {
+		url := "http://localhost:8080/fapi/update_daily_deposit_withdraw_metrics"
+		method := "POST"
 
-	payload := strings.NewReader(`{
-    "date": "2023-12-01"
-}`)
+		// Create the payload with the current date
+		payload := fmt.Sprintf(`{
+		    "date": "%s"
+		}`, date.Format("2006-01-02"))
 
-	client := &http.Client{}
-	req, err := http.NewRequest(method, url, payload)
+		client := &http.Client{}
+		req, err := http.NewRequest(method, url, bytes.NewBufferString(payload))
 
-	if err != nil {
-		fmt.Println(err)
-		return
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		req.Header.Add("Content-Type", "application/json")
+
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		defer res.Body.Close()
+
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Printf("Response for %s: %s\n", date.Format("2006-01-02"), string(body))
 	}
-	req.Header.Add("Content-Type", "application/json")
-
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer res.Body.Close()
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(string(body))
 }
