2a5b44e3d619bd69b233882adc9b6b11824fed7d
tunogya <20251942+tunogya@users.noreply.github.com>
2023-12-28 18:44:03 +0800

    update

--- scripts/jobs.go
+++ scripts/jobs.go
@@ -17,7 +17,8 @@ func main() {
 	for date := startDate; date.Before(endDate); date = date.AddDate(0, 0, 1) {
 		//url := "http://localhost:8080/fapi/update_daily_deposit_withdraw_metrics"
 		//url := "http://localhost:8080/fapi/update_daily_burn_metrics"
-		url := "http://localhost:8080/fapi/update_daily_futures_metrics"
+		//url := "http://localhost:8080/fapi/update_daily_futures_metrics"
+		url := "http://localhost:8080/api/summary/update_daily_trade_metrics"
 		method := "POST"
 
 		// Create the payload with the current date
