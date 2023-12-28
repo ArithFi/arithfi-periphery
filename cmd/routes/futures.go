cbd1250fccc60fe49ea71bf83a80676330be021b
tunogya <20251942+tunogya@users.noreply.github.com>
2023-12-28 13:49:14 +0800

    update

--- cmd/routes/futures.go
+++ cmd/routes/futures.go
@@ -1,8 +1,8 @@
 package routes
 
 import (
-	"github.com/arithfi/arithfi-periphery/internal/futures/account_trades"
 	"github.com/arithfi/arithfi-periphery/internal/futures/market_data"
+	"github.com/arithfi/arithfi-periphery/internal/futures/summary"
 	"github.com/labstack/echo/v4"
 )
 
@@ -10,18 +10,8 @@ func FuturesRoutes(e *echo.Echo) {
 	// Market data
 	e.GET("fapi/ping", market_data.Ping)
 	e.GET("fapi/time", market_data.Time)
-	//e.GET("exchangeInfo", market_data.ExchangeInfo)
-	//e.GET("kline", market_data.Kline)
-	//e.GET("ticker/24hr", market_data.Ticker24hr)
-	//e.GET("ticker/price", market_data.TickerPrice)
 
-	// Account trades
-	e.GET("fapi/order", account_trades.QueryOrder)
-	e.GET("fapi/orderAmendment", account_trades.GetOrderAmendment)
-	e.GET("fapi/openOrder", account_trades.QueryCurrentOpenOrder)
-	e.GET("fapi/openOrders", account_trades.QueryCurrentOpenOrders)
-	e.GET("fapi/account", account_trades.GetAccount)
-	e.GET("fapi/balance", account_trades.GetBalance)
-	e.GET("fapi/userTrades", account_trades.GetTradeList)
-	e.GET("fapi/allOrders", account_trades.GetAllOrders)
+	e.POST("fapi/update_daily_deposit_withdraw_metrics", summary.UpdateDailyDepositWithdrawMetrics)
+	e.POST("fapi/update_daily_destroy_metrics", summary.UpdateDailyFuturesDestroyMetrics)
+	e.POST("fapi/update_daily_futures_metrics", summary.UpdateDailyFuturesMetrics)
 }
--- internal/futures/summary/update_daily_deposit_withdraw_metrics.go
+++ internal/futures/summary/update_daily_deposit_withdraw_metrics.go
@@ -1,5 +1,10 @@
 package summary
 
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
 // 每日的充值和提现汇总数据
 // 用户、总充值、总提现
 /*
@@ -10,3 +15,18 @@ package summary
     AND orderType IN ('DEPOSIT', 'WALLET_DEPOSIT')
   GROUP BY walletAddress
 */
+
+type (
+	UpdateDailyDepositWithdrawMetricsReqType struct {
+		Date string `query:"date" form:"date" json:"date"` // Symbol
+	}
+)
+
+/*
+UpdateDailyDepositWithdrawMetrics UpdateDailyDepositWithdrawMetrics
+Weight: 1
+Parameters: NONE
+*/
+func UpdateDailyDepositWithdrawMetrics(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]string{})
+}
