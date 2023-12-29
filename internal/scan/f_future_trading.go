6696b61ef74aa6b94007f9379dab2d87a6dd0cd2
tunogya <20251942+tunogya@users.noreply.github.com>
2023-12-29 17:43:13 +0800

    update

--- internal/scan/f_future_trading.go
+++ internal/scan/f_future_trading.go
@@ -19,7 +19,7 @@ func FFutureTrading(c echo.Context) error {
 
 	fmt.Println("f_future_trading last timestamp:", lastTimestamp)
 
-	query, err := mysql.MYSQL.Query(`SELECT product, positionIndex, leverage, orderType, mode, direction, margin, volume, fees, sellValue, walletAddress, kolAddress, availableBanlance, copyAccountBalance
+	query, err := mysql.MYSQL.Query(`SELECT product, positionIndex, leverage, orderType, mode, direction, margin, volume, sellValue, walletAddress, kolAddress, availableBanlance, copyAccountBalance
 FROM f_future_trading 
 WHERE timestamp > ? 
 ORDER By timestamp 
@@ -40,39 +40,55 @@ LIMIT 100
 		var direction string
 		var margin float64
 		var volume float64
-		var fees float64
 		var sellValue float64
 		var walletAddress string
 		var kolAddress string
 		var availableBanlance float64
 		var copyAccountBalance float64
 
-		err := query.Scan(&product, &positionIndex, &leverage, &orderType, &mode, &direction, &margin, &volume, &fees, &sellValue, &walletAddress, &kolAddress, &availableBanlance, &copyAccountBalance)
+		err := query.Scan(&product, &positionIndex, &leverage, &orderType, &mode, &direction, &margin, &volume, &sellValue, &walletAddress, &kolAddress, &availableBanlance, &copyAccountBalance)
 		if err != nil {
 			return err
 		}
-
-		fmt.Println("f_future_trading:", product, positionIndex, leverage, orderType, mode, direction, margin, volume, fees, sellValue, walletAddress, kolAddress, availableBanlance, copyAccountBalance)
 
 		// 获取时间戳，需要处理成+8的北京时间,获取北京的时间的日期字符串
 		date := time.Unix(int64(timeStamp)+8*60*60, 0).Format("2006-01-02")
 		fmt.Println("date:", date)
 		if orderType == "MARKET_ORDER_FEE" || orderType == "LIMIT_ORDER_FEE" {
 			// 处理每个用户每天开单的数据汇总，新增仓位，开单数量
-			handleNewOrder(mode)
+			handleNewOrder(mode, date, walletAddress, kolAddress, volume)
 		} else if orderType == "MARKET_CLOSE_FEE" || orderType == "TP_ORDER_FEE" || orderType == "SL_ORDER_FEE" || orderType == "MARKET_LIQUIDATION" {
 			// 处理每个每天的净销毁
-			handleBurn(mode)
+			handleBurn(mode, sellValue, margin, walletAddress, kolAddress, date)
 		}
 	}
 
 	return nil
 }
 
-func handleNewOrder(mode string) {
-
+func handleNewOrder(mode string, date string, walletAddress string, kolAddress string, volume float64) {
+	_, err := mysql.MYSQL.Exec(`
+INSERT INTO b_daily_offchain_futures_metrics (date, walletAddress, mode, kolAddress, new_position_counts, new_position_size)
+VALUES (?, ?, ?, ?, ?, ?)
+ON DUPLICATE KEY UPDATE new_position_counts = new_position_counts + 1, new_position_size = new_position_size + ?
+`, date, walletAddress, mode, kolAddress, 1, volume, volume)
+	if err != nil {
+		fmt.Println("handleNewOrder err:", err)
+		return
+	}
+	fmt.Println("handleNewOrder ok")
 }
 
-func handleBurn(mode string) {
-
+func handleBurn(mode string, sellValue float64, margin float64, walletAddress string, kolAddress string, date string) {
+	var netBurnAmount = sellValue - margin
+	_, err := mysql.MYSQL.Exec(`
+INSERT INTO b_daily_offchain_futures_metrics (date, walletAddress, mode, kolAddress, net_burn_amount)
+VALUES (?, ?, ?, ?, ?)
+ON DUPLICATE KEY UPDATE net_burn_amount = VALUES(net_burn_amount)
+`, date, walletAddress, mode, kolAddress, netBurnAmount)
+	if err != nil {
+		fmt.Println("handleBurn err:", err)
+		return
+	}
+	fmt.Println("handleBurn ok")
 }
