783e8062968e85e67659d745fe8ed1f1b6e89ce1
tunogya <20251942+tunogya@users.noreply.github.com>
2024-01-04 18:58:05 +0800

    update

--- scripts/tokenholder_snapshot_create.go
+++ scripts/tokenholder_snapshot_create.go
@@ -6,12 +6,15 @@ import (
 	"github.com/arithfi/arithfi-periphery/configs/mongo"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"strconv"
+	"math/big"
 	"time"
 )
 
 func main() {
 	var fromBlock = "0"
+	totalSupply := new(big.Float)
+	totalSupply.SetString("300000000")
+
 	ctx := context.TODO()
 
 	opts := options.Find()
@@ -19,8 +22,9 @@ func main() {
 	opts.SetLimit(200)
 
 	// 维护一个map，用于存储每个地址的总额
-	balancesMap := make(map[string]float64)
-	snapshotMap := make(map[string]map[string]float64)
+	balancesMap := make(map[string]*big.Float)
+	snapshotMap := make(map[string]map[string]*big.Float)
+	totalTransfersMap := make(map[string]int)
 
 	// 每次更新 snapshot 后更新 snapshotCursorDate
 	var snapshotCursorDate = ""
@@ -45,6 +49,7 @@ func main() {
 				return
 			}
 			date := aggregate["date"].(string)
+			totalTransfersMap[date]++
 			abstract, ok := log["abstract"].(bson.M)
 			if !ok {
 				fmt.Println(log["abstract"])
@@ -53,21 +58,43 @@ func main() {
 			}
 			from := abstract["from"].(string)
 			to := abstract["to"].(string)
-			amount, err := strconv.ParseFloat(abstract["amount"].(string), 64)
-			if err != nil {
-				fmt.Println(err)
+			// aggregate["amount"]
+
+			// 使用 *big.Float 处理大数
+			amount, ok := new(big.Float).SetString(aggregate["amount"].(string))
+			if !ok {
+				fmt.Println(aggregate["amount"])
+				fmt.Println("无法获取amount字段或者amount字段不是字符串类型")
 				return
 			}
 			if from != "0x0000000000000000000000000000000000000000" {
-				balancesMap[from] -= amount
+				balancesMap[from].Sub(balancesMap[from], amount)
 			}
-			balancesMap[to] += amount
+			balancesMap[to].Add(balancesMap[to], amount)
 			snapshotMap[date] = balancesMap
 
 			if date > snapshotCursorDate {
 				// 准备更新snapshot
 				fmt.Println("准备更新snapshot", date)
 				fmt.Println(snapshotMap[date])
+				// 需要将snapshotMap[date]转换成数组,获取每个地址的余额
+				// [{address: '', quantity: '', percentage: ''}, {address: '', balance: '', percentage: ''}, ...]
+				var snapshotArray []bson.M
+				for address, balance := range snapshotMap[date] {
+					snapshotArray = append(snapshotArray, bson.M{"address": address, "quantity": balance, "percentage": new(big.Float).Quo(balance, totalSupply)})
+				}
+
+				// 定义一个摘要字段
+				var abstract bson.M
+				abstract["holders"] = len(snapshotArray)
+				abstract["total_transfers"] = totalTransfersMap[date]
+
+				// 插入到数据库，chain-bsc.tokenholder-snapshot
+				collection := mongo.MONGODB.Database("chain-bsc").Collection("tokenholder-snapshot")
+				_, err := collection.InsertOne(ctx, bson.M{"date": date, "abstract": abstract, "holders": snapshotArray})
+				if err != nil {
+					fmt.Println(err)
+				}
 				snapshotCursorDate = date
 			}
 		}
