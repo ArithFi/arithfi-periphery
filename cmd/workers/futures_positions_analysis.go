81878b68756730f1a967311454a429de820eb667
tunogya <20251942+tunogya@users.noreply.github.com>
2024-01-10 12:48:26 +0800

    update

---
+++ cmd/workers/futures_positions_analysis.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"github.com/arithfi/arithfi-periphery/configs/mongo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"math/big"
+	"strings"
+	"time"
+)
+
+// This code can run continuously.
+// The function is to retrieve transfer information from MongoDB, and analyze and expand the fields of the information.
+
+func main() {
+	var fromPositionIndex = int64(-1)
+	ctx := context.TODO()
+
+	opts := options.Find()
+	opts.SetSort(bson.D{{"positionIndex", 1}}) // 按照blockNumber升序排序
+	opts.SetLimit(1000)
+
+	collection := mongo.MONGODB.Database("chain-bsc").Collection("futures-positions")
+
+	for {
+		cursor, err := collection.Find(ctx, bson.M{"blockNumber": bson.M{"$gt": fromPositionIndex}}, opts)
+		if err != nil {
+			return
+		}
+		defer cursor.Close(ctx)
+
+		for cursor.Next(ctx) {
+			var position bson.M
+			if err := cursor.Decode(&position); err != nil {
+				continue
+			}
+			timestamp := new(big.Int)
+			timestamp.SetString(strings.TrimPrefix(position["timestamp"].(string), "0x"), 16)
+			loc, err := time.LoadLocation("Asia/Shanghai")
+			if err != nil {
+				log.Println("Error loading location:", err)
+				return
+			}
+			date := time.Unix(timestamp.Int64(), 0).In(loc).Format("2006-01-02")
+			aggregate := bson.M{
+				"date":     date,
+				"location": "Asia/Shanghai",
+			}
+			position["aggregate"] = aggregate
+
+			_, err = collection.UpdateOne(ctx, bson.M{"_id": position["_id"]}, bson.M{"$set": position})
+			if err != nil {
+				return
+			}
+
+			log.Println("Update transfer_logs success, positionIndex:", position["positionIndex"], ", date:", date)
+			fromPositionIndex = position["positionIndex"].(int64)
+		}
+		log.Println("Sleep 10 seconds")
+		time.Sleep(time.Second * 10)
+	}
+}
--- cmd/workers/futures_positions_retrieve.go
+++ cmd/workers/futures_positions_retrieve.go
@@ -7,6 +7,7 @@ import (
 	"github.com/arithfi/arithfi-periphery/internal/offchain"
 	"go.mongodb.org/mongo-driver/bson"
 	"log"
+	"time"
 )
 
 func main() {
@@ -22,175 +23,176 @@ func main() {
 			log.Println("Scan Futures Tradings err", err)
 			continue
 		}
-		if len(actions) == 0 {
-			log.Println("Scan Futures Tradings empty")
-			break
-		}
-		log.Println("len(actions):", len(actions))
-		for _, action := range actions {
-			switch action.OrderType {
-			case "MARKET_ORDER_FEE":
-				_, err := futuresPositionsCollection.InsertOne(ctx, bson.D{
-					{"positionIndex", action.PositionIndex},
-					{"product", action.Product},
-					{"positionStatus", "open"},
-					{"timestamp", action.Timestamp},
-					{"leverage", action.Leverage},
-					{"positionSize", float64(action.Leverage) * action.Margin},
-					{"mode", action.Mode},
-					{"direction", action.Direction},
-					{"margin", action.Margin},
-					{"initialMargin", action.Margin},
-					{"walletAddress", action.WalletAddress},
-					{"kolAddress", action.KolAddress},
-					{"openFees", action.Fees},
-					{"openPrice", action.OrderPrice},
-				})
-				if err != nil {
-					log.Println("MARKET_ORDER_FEE:", err)
+		if len(actions) > 0 {
+			for _, action := range actions {
+				switch action.OrderType {
+				case "MARKET_ORDER_FEE":
+					_, err := futuresPositionsCollection.InsertOne(ctx, bson.D{
+						{"positionIndex", action.PositionIndex},
+						{"product", action.Product},
+						{"positionStatus", "open"},
+						{"timestamp", action.Timestamp},
+						{"leverage", action.Leverage},
+						{"positionSize", float64(action.Leverage) * action.Margin},
+						{"mode", action.Mode},
+						{"direction", action.Direction},
+						{"margin", action.Margin},
+						{"initialMargin", action.Margin},
+						{"walletAddress", action.WalletAddress},
+						{"kolAddress", action.KolAddress},
+						{"openFees", action.Fees},
+						{"openPrice", action.OrderPrice},
+					})
+					if err != nil {
+						log.Println("MARKET_ORDER_FEE:", err)
+						break
+					}
+					log.Println("MARKET_ORDER_FEE:", action.Id)
+					break
+				case "MARKET_CLOSE_FEE":
+					_, err := futuresPositionsCollection.UpdateOne(
+						ctx,
+						bson.M{"positionIndex": action.PositionIndex},
+						bson.M{"$set": bson.M{"closeFees": action.Fees, "positionStatus": "closed", "sellValue": action.SellValue, "closePrice": action.OrderPrice}},
+					)
+					if err != nil {
+						log.Println("MARKET_CLOSE_FEE:", err)
+						break
+					}
+					log.Println("MARKET_CLOSE_FEE:", action.Id)
+					break
+				case "LIMIT_CANCEL":
+					_, err := futuresPositionsCollection.UpdateOne(
+						ctx,
+						bson.M{"positionIndex": action.PositionIndex},
+						bson.M{"$set": bson.M{"positionStatus": "cancelled"}},
+					)
+					if err != nil {
+						log.Println("LIMIT_CANCEL:", err)
+						break
+					}
+					log.Println("LIMIT_CANCEL:", action.Id)
+					break
+				case "LIMIT_EDIT":
+					_, err := futuresPositionsCollection.UpdateOne(
+						ctx,
+						bson.M{"positionIndex": action.PositionIndex},
+						bson.M{"$set": bson.M{"entryPrice": action.OrderPrice}},
+					)
+					if err != nil {
+						log.Println("LIMIT_EDIT:", err)
+						break
+					}
+					log.Println("LIMIT_EDIT:", action.Id)
+					break
+				case "LIMIT_ORDER_FEE":
+					_, err := futuresPositionsCollection.UpdateOne(
+						ctx,
+						bson.M{"positionIndex": action.PositionIndex},
+						bson.M{"$set": bson.M{
+							"openPrice":      action.OrderPrice,
+							"openFees":       action.Fees,
+							"positionStatus": "open",
+						}},
+					)
+					if err != nil {
+						log.Println("LIMIT_ORDER_FEE:", err)
+						break
+					}
+					log.Println("LIMIT_ORDER_FEE:", action.Id)
+					break
+				case "LIMIT_REQUEST":
+					_, err := futuresPositionsCollection.InsertOne(ctx, bson.D{
+						{"positionIndex", action.PositionIndex},
+						{"product", action.Product},
+						{"positionStatus", "pending"},
+						{"timestamp", action.Timestamp},
+						{"leverage", action.Leverage},
+						{"positionSize", float64(action.Leverage) * action.Margin},
+						{"mode", action.Mode},
+						{"direction", action.Direction},
+						{"margin", action.Margin},
+						{"initialMargin", action.Margin},
+						{"walletAddress", action.WalletAddress},
+						{"kolAddress", action.KolAddress},
+						{"entryPrice", action.OrderPrice},
+					})
+					if err != nil {
+						log.Println("LIMIT_REQUEST:", err)
+						break
+					}
+					log.Println("LIMIT_REQUEST:", action.PositionIndex)
+					break
+				case "MARKET_LIQUIDATION":
+					_, err := futuresPositionsCollection.UpdateOne(
+						ctx,
+						bson.M{"positionIndex": action.PositionIndex},
+						bson.M{"$set": bson.M{"positionSize": 0, "positionStatus": "closed", "closeFees": 0, "sellValue": 0, "closePrice": action.OrderPrice}},
+					)
+					if err != nil {
+						log.Println("MARKET_LIQUIDATION:", err)
+						break
+					}
+					log.Println("MARKET_LIQUIDATION:", action.Id)
+					break
+				case "MARKET_ORDER_ADD":
+					_, err := futuresPositionsCollection.UpdateOne(
+						ctx,
+						bson.M{"positionIndex": action.PositionIndex},
+						bson.M{"$set": bson.M{"margin": action.Margin}},
+					)
+					if err != nil {
+						log.Println("MARKET_ORDER_ADD:", err)
+						break
+					}
+					log.Println("MARKET_ORDER_ADD:", action.Id)
+					break
+				case "SL_ORDER_FEE":
+					_, err := futuresPositionsCollection.UpdateOne(
+						ctx,
+						bson.M{"positionIndex": action.PositionIndex},
+						bson.M{"$set": bson.M{"positionStatus": "closed", "closeFees": action.Fees, "sellValue": action.SellValue, "closePrice": action.OrderPrice}},
+					)
+					if err != nil {
+						log.Println("SL_ORDER_FEE:", err)
+						break
+					}
+					log.Println("SL_ORDER_FEE:", action.Id)
+					break
+				case "TP_ORDER_FEE":
+					_, err := futuresPositionsCollection.UpdateOne(
+						ctx,
+						bson.M{"positionIndex": action.PositionIndex},
+						bson.M{"$set": bson.M{"positionStatus": "closed", "closeFees": action.Fees, "sellValue": action.SellValue, "closePrice": action.OrderPrice}},
+					)
+					if err != nil {
+						log.Println("TP_ORDER_FEE:", err)
+						break
+					}
+					log.Println("TP_ORDER_FEE:", action.Id)
+					break
+				case "TPSL_EDIT":
+					_, err := futuresPositionsCollection.UpdateOne(
+						ctx,
+						bson.M{"positionIndex": action.PositionIndex},
+						bson.M{"$set": bson.M{"stopLossPrice": action.StopLossPrice, "takeProfitPrice": action.TakeProfitPrice}},
+					)
+					if err != nil {
+						log.Println("TPSL_EDIT:", err)
+						break
+					}
+					log.Println("TPSL_EDIT:", action.Id)
+					break
+				default:
+					fmt.Println(action.OrderType, action.Id)
 					break
 				}
-				log.Println("MARKET_ORDER_FEE:", action.Id)
-				break
-			case "MARKET_CLOSE_FEE":
-				_, err := futuresPositionsCollection.UpdateOne(
-					ctx,
-					bson.M{"positionIndex": action.PositionIndex},
-					bson.M{"$set": bson.M{"closeFees": action.Fees, "positionStatus": "closed", "sellValue": action.SellValue, "closePrice": action.OrderPrice}},
-				)
-				if err != nil {
-					log.Println("MARKET_CLOSE_FEE:", err)
-					break
-				}
-				log.Println("MARKET_CLOSE_FEE:", action.Id)
-				break
-			case "LIMIT_CANCEL":
-				_, err := futuresPositionsCollection.UpdateOne(
-					ctx,
-					bson.M{"positionIndex": action.PositionIndex},
-					bson.M{"$set": bson.M{"positionStatus": "cancelled"}},
-				)
-				if err != nil {
-					log.Println("LIMIT_CANCEL:", err)
-					break
-				}
-				log.Println("LIMIT_CANCEL:", action.Id)
-				break
-			case "LIMIT_EDIT":
-				_, err := futuresPositionsCollection.UpdateOne(
-					ctx,
-					bson.M{"positionIndex": action.PositionIndex},
-					bson.M{"$set": bson.M{"entryPrice": action.OrderPrice}},
-				)
-				if err != nil {
-					log.Println("LIMIT_EDIT:", err)
-					break
-				}
-				log.Println("LIMIT_EDIT:", action.Id)
-				break
-			case "LIMIT_ORDER_FEE":
-				_, err := futuresPositionsCollection.UpdateOne(
-					ctx,
-					bson.M{"positionIndex": action.PositionIndex},
-					bson.M{"$set": bson.M{
-						"openPrice":      action.OrderPrice,
-						"openFees":       action.Fees,
-						"positionStatus": "open",
-					}},
-				)
-				if err != nil {
-					log.Println("LIMIT_ORDER_FEE:", err)
-					break
-				}
-				log.Println("LIMIT_ORDER_FEE:", action.Id)
-				break
-			case "LIMIT_REQUEST":
-				_, err := futuresPositionsCollection.InsertOne(ctx, bson.D{
-					{"positionIndex", action.PositionIndex},
-					{"product", action.Product},
-					{"positionStatus", "pending"},
-					{"timestamp", action.Timestamp},
-					{"leverage", action.Leverage},
-					{"positionSize", float64(action.Leverage) * action.Margin},
-					{"mode", action.Mode},
-					{"direction", action.Direction},
-					{"margin", action.Margin},
-					{"initialMargin", action.Margin},
-					{"walletAddress", action.WalletAddress},
-					{"kolAddress", action.KolAddress},
-					{"entryPrice", action.OrderPrice},
-				})
-				if err != nil {
-					log.Println("LIMIT_REQUEST:", err)
-					break
-				}
-				log.Println("LIMIT_REQUEST:", action.PositionIndex)
-				break
-			case "MARKET_LIQUIDATION":
-				_, err := futuresPositionsCollection.UpdateOne(
-					ctx,
-					bson.M{"positionIndex": action.PositionIndex},
-					bson.M{"$set": bson.M{"positionSize": 0, "positionStatus": "closed", "closeFees": 0, "sellValue": 0, "closePrice": action.OrderPrice}},
-				)
-				if err != nil {
-					log.Println("MARKET_LIQUIDATION:", err)
-					break
-				}
-				log.Println("MARKET_LIQUIDATION:", action.Id)
-				break
-			case "MARKET_ORDER_ADD":
-				_, err := futuresPositionsCollection.UpdateOne(
-					ctx,
-					bson.M{"positionIndex": action.PositionIndex},
-					bson.M{"$set": bson.M{"margin": action.Margin}},
-				)
-				if err != nil {
-					log.Println("MARKET_ORDER_ADD:", err)
-					break
-				}
-				log.Println("MARKET_ORDER_ADD:", action.Id)
-				break
-			case "SL_ORDER_FEE":
-				_, err := futuresPositionsCollection.UpdateOne(
-					ctx,
-					bson.M{"positionIndex": action.PositionIndex},
-					bson.M{"$set": bson.M{"positionStatus": "closed", "closeFees": action.Fees, "sellValue": action.SellValue, "closePrice": action.OrderPrice}},
-				)
-				if err != nil {
-					log.Println("SL_ORDER_FEE:", err)
-					break
-				}
-				log.Println("SL_ORDER_FEE:", action.Id)
-				break
-			case "TP_ORDER_FEE":
-				_, err := futuresPositionsCollection.UpdateOne(
-					ctx,
-					bson.M{"positionIndex": action.PositionIndex},
-					bson.M{"$set": bson.M{"positionStatus": "closed", "closeFees": action.Fees, "sellValue": action.SellValue, "closePrice": action.OrderPrice}},
-				)
-				if err != nil {
-					log.Println("TP_ORDER_FEE:", err)
-					break
-				}
-				log.Println("TP_ORDER_FEE:", action.Id)
-				break
-			case "TPSL_EDIT":
-				_, err := futuresPositionsCollection.UpdateOne(
-					ctx,
-					bson.M{"positionIndex": action.PositionIndex},
-					bson.M{"$set": bson.M{"stopLossPrice": action.StopLossPrice, "takeProfitPrice": action.TakeProfitPrice}},
-				)
-				if err != nil {
-					log.Println("TPSL_EDIT:", err)
-					break
-				}
-				log.Println("TPSL_EDIT:", action.Id)
-				break
-			default:
-				fmt.Println(action.OrderType, action.Id)
-				break
 			}
+			lastId = actions[len(actions)-1].Id
+		} else {
+			log.Println("Scan Futures Tradings empty")
 		}
-		lastId = actions[len(actions)-1].Id
+		log.Println("Sleep 10 seconds")
+		time.Sleep(time.Second * 10)
 	}
 }
