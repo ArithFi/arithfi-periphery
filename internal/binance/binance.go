a5416b8976f3cc77bedf28fdb95f7e4461c78a3f
tunogya <20251942+tunogya@users.noreply.github.com>
2024-01-16 17:08:24 +0800

    update

--- internal/binance/binance.go
+++ internal/binance/binance.go
@@ -19,6 +19,7 @@ const (
 func GetKlines(symbol string, interval string, startTime int64, endTime int64) *[]model.Kline {
 	cache := getFromCache(symbol, interval, startTime/1000, endTime/1000)
 	if cache != nil {
+		log.Println("cache hit", symbol, interval, startTime, endTime)
 		return cache
 	}
 
@@ -58,6 +59,7 @@ func GetKlines(symbol string, interval string, startTime int64, endTime int64) *
 						log.Println(err)
 					}
 				}()
+				log.Println("api hit", symbol, interval, startTime, endTime)
 				return &totalKlines
 			}
 		}
@@ -112,7 +114,7 @@ func cacheKlines(exchangeInfo *[]model.Kline, resolution string, symbol string) 
 func getFromCache(symbol string, interval string, startTime int64, endTime int64) *[]model.Kline {
 	result, _ := mysql.ArithFiDB.Query("select timestamp, open, high, low, close, volume from kline_cache where symbol = ? and resolution = ? and timestamp >= ? and timestamp < ? order by timestamp", symbol, interval, startTime, endTime)
 
-	exchangeInfo := make([]model.Kline, 0)
+	totalKlines := make([]model.Kline, 0)
 	for result.Next() {
 		var data model.Kline
 		err := result.Scan(&data.OpenTime, &data.Open, &data.High, &data.Low, &data.Close, &data.Volume)
@@ -120,10 +122,32 @@ func getFromCache(symbol string, interval string, startTime int64, endTime int64
 			log.Println("Get from cache error")
 			return nil
 		}
-		exchangeInfo = append(exchangeInfo, data)
+		totalKlines = append(totalKlines, data)
 	}
 
-	return &exchangeInfo
+	var count = (startTime - endTime) / (IntervalMap[interval] * 1000)
+	if float64(len(totalKlines)) < float64(int(count))*0.9 {
+		return nil
+	}
+	return &totalKlines
+}
+
+var IntervalMap = map[string]int64{
+	"1m":  60,
+	"3m":  180,
+	"5m":  300,
+	"15m": 900,
+	"30m": 1800,
+	"1h":  3600,
+	"2h":  7200,
+	"4h":  14400,
+	"6h":  21600,
+	"8h":  28800,
+	"12h": 43200,
+	"1d":  86400,
+	"3d":  259200,
+	"1w":  604800,
+	"1M":  2592000,
 }
 
 func requestAPI(endpoint string) []byte {
