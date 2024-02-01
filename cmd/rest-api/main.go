c5ca32cfeaccacee73f6a22ea9616fcf0e84c7fc
tunogya <20251942+tunogya@users.noreply.github.com>
2024-02-01 21:01:54 +0800

    update

--- cmd/rest-api/main.go
+++ cmd/rest-api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "github.com/arithfi/arithfi-periphery/configs"
+	"github.com/arithfi/arithfi-periphery/internal/bscscan"
 	"github.com/arithfi/arithfi-periphery/internal/datafeed"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -29,6 +30,7 @@ func main() {
 	e.GET("/config", datafeed.GetConfig)
 	e.GET("/symbols", datafeed.Symbols)
 	e.GET("/history", datafeed.History)
+	e.GET("/tag", bscscan.GetTag)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
