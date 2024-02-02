3d3abe3de08c924e0c50a90d23eac38c10d96d1c
tunogya <20251942+tunogya@users.noreply.github.com>
2024-02-02 10:33:51 +0800

    Revert "update"
    This reverts commit c5ca32cfeaccacee73f6a22ea9616fcf0e84c7fc.

--- cmd/rest-api/main.go
+++ cmd/rest-api/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	_ "github.com/arithfi/arithfi-periphery/configs"
-	"github.com/arithfi/arithfi-periphery/internal/bscscan"
 	"github.com/arithfi/arithfi-periphery/internal/datafeed"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -30,7 +29,6 @@ func main() {
 	e.GET("/config", datafeed.GetConfig)
 	e.GET("/symbols", datafeed.Symbols)
 	e.GET("/history", datafeed.History)
-	e.GET("/tag", bscscan.GetTag)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
